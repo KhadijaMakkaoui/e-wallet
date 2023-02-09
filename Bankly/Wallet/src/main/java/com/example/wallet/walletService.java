8422535c2848c6a9f2b44d45d3636b9475c669fb
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-02-09 09:27:34 +0100

    delete unused code

--- Bankly/Wallet/src/main/java/com/example/wallet/walletService.java
+++ Bankly/Wallet/src/main/java/com/example/wallet/walletService.java
@@ -1,5 +1,8 @@
 package com.example.wallet;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,19 +11,23 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class walletService {
-    @Autowired
-    private walletRepository walletRepository;
-    @Autowired
-    private MapperDTO mapper;
+    private final walletRepository walletRepository;
+    private final MapperDTO mapper;
     public List<walletDTO> getAll() {
         return walletRepository.findAll()
                 .stream()
                 .map((wallet) -> mapper.mapToDTO(wallet))
                 .collect(toList());
     }
-
+    /*@SneakyThrows*/
     public wallet checkBallance(String ref, float amount) {
+       /* log.info("Wait started");
+            Thread.sleep(10000);
+
+        log.info("Wait ended");*/
         wallet wallet = walletRepository.findByRef(ref);
         if (wallet.getBalance()>=amount) {
             return wallet;
