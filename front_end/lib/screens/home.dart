517f887fa76160194beca9e9915a238350e82c18
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-02 09:24:08 +0100

    create application Bar

--- front_end/lib/screens/home.dart
+++ front_end/lib/screens/home.dart
@@ -9,6 +9,51 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: const AssetImage('assets/person.jpg'),
+              radius: 20,
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Bonjour,',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize:18,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                Text(
+                  'Khadija Makkaoui',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+
+              ],
+            ),
+
+
+
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+        elevation: 0,
+      ),
+      body: Container(
+        color: Colors.lightBlue[600],
+      ),
+
+
+    );
   }
 }
